abd79dc1cd4955495fb5a9f449d8b5a735490bdb
Minh <kobrighto@gmail.com>
2014-06-10 21:44:18 +0900

    Index + Search for StandardAnalyzer

---
+++ CS660_Lucene/src/org/apache/lucene/demo/IndexHTML.java
@@ -0,0 +1,228 @@
+package org.apache.lucene.demo;
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import kaist.irproject.lucene.analysis.PorterStemAnalyzer;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Indexer for HTML files.
+ * Modified by T.Y.Tung (2009/06/22) for NTU 2009 term project
+ */
+public class IndexHTML {
+	private IndexHTML() {
+	}
+
+	private static boolean deleting = false; // true during deletion pass
+	private static IndexReader reader; // existing index
+	private static IndexWriter writer; // new index being built
+	
+//	private static PageRanker pageRanker = null; // for PageRank
+
+	/** Indexer for HTML files. */
+	public static void main(String[] args) {
+		try {
+			String index = "index";
+			boolean create = false;
+			File root = null;
+
+			//String usage = "IndexHTML [-create] [-index <index>] <root_directory>";
+			
+			String usage =  "-index indexHTML_StandardAnalyzer -create WT10G";
+			String[] argv = usage.split (" ");
+
+			if (argv.length == 0) {
+				System.err.println("Usage: " + usage);
+				return;
+			}
+
+			for (int i = 0; i < argv.length; i++) {
+				if (argv[i].equals("-index")) { // parse -index option
+					index = argv[++i];
+				} else if (argv[i].equals("-create")) { // parse -create option
+					create = true;
+				} else if (i != argv.length - 1) {
+					System.err.println("Usage: " + usage);
+					return;
+				} else
+					root = new File(argv[i]);
+			}
+			Date start = new Date();
+
+			if (!create) { // delete stale docs
+				deleting = true;				
+				indexDocs(root, index, create);
+			}
+			// modified by T.Y.Tung => PorterStemAnalyzer(stopwords)
+			//File stopwords = new File("ftp.cs.cornell.edu_pub_smart_english.stop");
+			//Analyzer analyzer = new PorterStemAnalyzer();
+			//writer = new IndexWriter(index, analyzer, create, new IndexWriter.MaxFieldLength(1000000));
+			
+			Directory dir = FSDirectory.open(new File(index));
+			Analyzer indexAnalyzer = new StandardAnalyzer(Version.LUCENE_48);
+			IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_48, indexAnalyzer);				
+			writer = new IndexWriter(dir, iwc);
+			
+//			pageRanker = new PageRanker(); // for PageRank
+			
+			indexDocs(root, index, create); // add new docs
+
+			//System.out.println("Optimizing index...");
+			//writer.optimize();
+			writer.close();
+
+			Date end = new Date();
+
+			System.out.print(end.getTime() - start.getTime());
+			System.out.println(" total milliseconds");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(" caught a " + e.getClass()
+					+ "\n with message: " + e.getMessage());
+		}
+	}
+
+	/*
+	 * Walk directory hierarchy in uid order, while keeping uid iterator from /*
+	 * existing index in sync. Mismatches indicate one of: (a) old documents to
+	 * /* be deleted; (b) unchanged documents, to be left alone; or (c) new /*
+	 * documents, to be indexed.
+	 */
+
+	private static void indexDocs(File file, String index, boolean create)
+			throws Exception {
+		/*if (!create) { // incrementally update
+
+			reader = IndexReader.open(index); // open existing index
+
+			indexDocs(file);
+
+			if (deleting) { // delete rest of stale docs
+				deleting = false;
+			}
+			
+			reader.close(); // close existing index
+
+		} else*/
+			// don't have exisiting
+			indexDocs(file);
+	}
+
+	private static void indexDocs(File file) throws Exception {
+		if (file.isDirectory()) { // if a directory
+			String[] files = file.list(); // list its files
+			Arrays.sort(files); // sort the files
+			for (int i = 0; i < files.length; i++)
+				// recursively index them
+				indexDocs(new File(file, files[i]));
+
+		} // modified by T.Y.Tung
+		else if (file.getPath().substring(file.getPath().lastIndexOf("\\")+1).startsWith("B") || // index TREC training data
+				file.getPath().endsWith(".html") || // index .html files
+				file.getPath().endsWith(".htm") || // index .htm files
+				file.getPath().endsWith(".txt")) { // index .txt files
+			 // add a new index
+				 // modified by T.Y.Tung
+				System.out.println("adding " + file.getPath());
+				addDocuments(writer, file);
+		}
+	}
+
+	// added by T.Y.Tung
+	private static void addDocuments(IndexWriter writer, File file)
+			throws IOException, InterruptedException {
+		ArrayList<HashMap<String, String>> docsList = readDocs(file);
+		for (int i=0; i<docsList.size(); i++) {
+			HashMap<String, String> docsMap = docsList.get(i);
+			String docNo = docsMap.get("DOCNO");
+			String html = docsMap.get("DOC");
+			InputStream is = new ByteArrayInputStream(html.getBytes());
+			Document doc = HTMLDocument.Document(is, docNo);
+			// add PageRank weights to index
+//			boolean isPageRank = true; //quick debugging, it should be extracted to an input parameter
+//			if (isPageRank) {
+//				float score = pageRanker.getPageRank(docNo); //WTX010-B01-1 = 1.4805996E-6
+//				doc.setBoost(score*1000*1000);
+//				float boost = doc.getBoost();
+//				//System.out.println("PageRank score for "+docNo+":\t"+boost);
+//			}
+			//System.out.println(doc.get("DOCNO"));
+			//System.out.println(doc.get("contents"));
+			//System.out.println(writer.toString());
+			
+			writer.addDocument(doc);
+		}
+	}
+
+	// added by T.Y.Tung
+	public static ArrayList<HashMap<String, String>> readDocs(File file) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		ArrayList<HashMap<String, String>> docsList = new ArrayList<HashMap<String, String>>();
+		try {
+			String line = null;
+			String sep = null;
+			String newline = System.getProperty("line.separator");
+			StringBuffer sb = null;
+			HashMap<String, String> docsMap = null;
+			while (null != (line=reader.readLine())) {
+				sep = newline;
+				if (line.equals("<DOC>")) {
+					sb = new StringBuffer();
+					docsMap = new HashMap<String, String>();
+					sep = "";
+				}
+				sb.append(sep+line);
+				if (line.startsWith("<DOCNO>")) {
+		    		int endIndex = line.indexOf("</DOCNO>");
+		    		String docNo = line.substring(7, endIndex);
+		    		docsMap.put("DOCNO", docNo);
+				}
+				if (line.equals("</DOC>")) {
+					docsMap.put("DOC", sb.toString());
+					docsList.add(docsMap);
+				}
+			}
+		} finally {
+			reader.close();
+		}
+		return docsList;
+	}
+
+}
---
+++ CS660_Lucene/src/org/apache/lucene/demo/SearchFiles.java
@@ -0,0 +1,292 @@
+package org.apache.lucene.demo;
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import ireval.*;
+import ireval.RetrievalEvaluator.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.TreeMap;
+import java.util.Vector;
+
+import kaist.irproject.lucene.analysis.PorterStemAnalyzer;
+import kaist.irproject.lucene.trec.QualityQuery;
+import kaist.irproject.lucene.trec.SubmissionReport;
+import kaist.irproject.lucene.trec.TrecTopicsReader;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.search.*;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+/**
+ * Simple command-line based search demo. 
+ * Modified by T.Y.Tung (2009/06/22) for NTU 2009 term project
+ */
+public class SearchFiles {
+
+	private SearchFiles() {
+	}
+
+	/** Simple command-line based search demo. */
+	public static void main(String[] argv) throws Exception {
+		String usage = "Usage:\tjava org.apache.lucene.demo.SearchFiles " +
+				"[-index dir] [-analyzer name] [-topics file] [-output file] " +
+				"[-WordNet yes-no] [-RelevanceFeedback intTimes docNum termNum]";
+		usage += "\n\n\t-analyzer [StandardAnalyzer | PorterStemAnalyzer]";
+		
+		usage = "-index indexHTML_StandardAnalyzer -analyzer StandardAnalyzer -topics dataset/testing_topics.txt "
+				+ "-output output/testing/testing_topics_result-[StandardAnalyzer].txt -WordNet no";
+		String[] args = usage.split(" ");
+				
+		if (args.length > 0
+				&& ("-h".equals(args[0]) || "-help".equals(args[0]))) {
+			System.out.println(usage);
+			System.exit(0);
+		}
+		if (args.length == 0) {
+			System.err.println(usage);
+			return;
+		}
+
+		String index = "indexHTML";
+		String analyzerName = "";
+		String topicsFile = null;
+		String rankingFile = "output.txt";
+		boolean isWordNet = false;
+		boolean isRelevanceFeedback = false;
+		int relevanceFeedbackRuns = 0;
+		String docNum = "";
+		String termNum = "";
+
+		for (int i = 0; i < args.length; i++) {
+			if ("-index".equals(args[i])) {
+				index = args[i + 1];
+				i++;
+			} else if ("-analyzer".equals(args[i])) {
+				analyzerName = args[i + 1];
+				i++;
+			} else if ("-topics".equals(args[i])) {
+				topicsFile = args[i + 1];
+				i++;
+			} else if ("-output".equals(args[i])) {
+				rankingFile = args[i + 1];
+				i++;
+			} else if ("-WordNet".equals(args[i])) {
+				if (args[i + 1].equals("yes"))
+					isWordNet = true;
+				i++;
+			} else if ("-RelevanceFeedback".equals(args[i])) {
+				isRelevanceFeedback = true;
+				try { // must
+					if (args[i + 1] != null)
+						relevanceFeedbackRuns = Integer.parseInt(args[i + 1]);
+				} catch (Exception e) {
+					System.err.println(usage);
+					return;
+				}					
+				try { // optional
+					if (args[i + 2] != null)
+						docNum = args[i + 2];
+					if (args[i + 3] != null)
+						termNum = args[i + 3];
+				} catch (Exception e) {
+				}
+				i++;
+			}
+		}
+		
+		//IndexReader reader = DirectoryReader.open(FSDirectory.open(new File(index)));
+		IndexReader reader = DirectoryReader.open(FSDirectory.open(new File(index)));
+		IndexSearcher searcher = new IndexSearcher(reader);
+		Analyzer analyzer = null;
+		// modified by T.Y.Tung
+		//File stopwords = new File("ftp.cs.cornell.edu_pub_smart_english.stop");
+		if (analyzerName.equals("PorterStemAnalyzer")) {
+			analyzer = new PorterStemAnalyzer();
+		} else if (analyzerName.equals("StandardAnalyzer")) {
+			analyzer = new StandardAnalyzer(Version.LUCENE_48);
+		} else {
+			System.err.println(usage);
+			return;
+		}
+
+		if (topicsFile != null) {
+			TrecTopicsReader qReader = new TrecTopicsReader();
+			QualityQuery qqs[] = qReader.readQueries(new BufferedReader(new FileReader(topicsFile)));
+//			for (int i=0; i<qqs.length; i++) {
+//				System.out.print(qqs[i].getQueryID());
+//				System.out.println(" "+qqs[i].getValue("title"));
+//				System.out.println("<description> "+qqs[i].getValue("description"));
+//				System.out.println("<narrative> "+qqs[i].getValue("narrative"));
+//				System.out.println("");
+//			}
+			
+			//QueryParser parser = new QueryParser("contents", analyzer);
+			String[] fields = {"contents", "title"};
+			Map<String, Float> boosts = new HashMap<String, Float>();
+			boosts.put(fields[0], Float.valueOf("1.2"));
+			boosts.put(fields[1], Float.valueOf("0.016"));
+			MultiFieldQueryParser parser = new MultiFieldQueryParser(Version.LUCENE_48, fields, analyzer, boosts);
+
+			// Rocchio's pseudo relevance feedback QueryExpansion
+			// TODO: alpha, beta, decay don't work because field 'contents' has no terms to be expanded
+			Properties props = new Properties();
+			props.load(new FileReader("src/query_expansion.prop"));
+//			props.setProperty("QE.method", "rocchio");
+//			props.setProperty("QE.decay", "0.04");
+			if (!docNum.equals("") && !termNum.equals("")) {
+				// command line parameters have higher priority than property file (query_expansion.prop)
+				props.setProperty("QE.doc.num", docNum);
+				props.setProperty("QE.term.num", termNum);
+			}
+//			props.setProperty("rocchio.alpha", "1");
+//			props.setProperty("rocchio.beta", "0.75");
+			
+			// for WordNet Query Expansion
+			/*IndexSearcher wnSearcher = new IndexSearcher("indexWordNet");
+			Analyzer wnAnalyzer = analyzer;*/
+			
+			File f = new File(rankingFile);
+			if (f.exists()) {
+				f.delete();
+			}
+			// Search
+			Query query = null;
+			Query expandedQuery = null; // for WordNet Query Expansion
+			TopDocs td = null;
+			for (int i=0; i<qqs.length; i++) {
+				String queryStr = qqs[i].getValue("title")+" "+qqs[i].getValue("description")+" "+qqs[i].getValue("narrative");
+				queryStr = queryStr.replaceAll("\\*", "").replaceAll("\\?", ""); // replace * and ?
+				// query of TREC topic
+				query = parser.parse(queryStr);
+				
+				// WordNet Query Expansion
+				/*if (isWordNet) {
+					// expanded query using WordNet
+					expandedQuery = SynExpand.expand(qqs[i].getValue("title"), wnSearcher, wnAnalyzer, "contents", 0.2f);
+					// combine the above two queries
+					((BooleanQuery) query).add(expandedQuery, BooleanClause.Occur.SHOULD);
+				}*/
+				
+				// First search
+				System.out.println("\nSearching for : (" + qqs[i].getQueryID()+") "+query);
+				td = doTopHitSearch(searcher, query, 100);
+				
+				// Rocchio's pseudo relevance feedback Query Expansion
+				if (isRelevanceFeedback) {
+					for (int k=1; k <= relevanceFeedbackRuns; k++) {
+						Vector<Document> hits = new Vector<Document>();
+						ScoreDoc[] sd = td.scoreDocs;
+						for (int j = 0; j < sd.length; j++) {
+							int docId = sd[j].doc;
+							Document doc = searcher.doc(docId);
+							hits.add(doc);
+						}
+		
+						// Rocchio's pseudo relevance feedback QueryExpansion
+						/*Similarity similarity = query.getSimilarity(searcher);
+						QueryExpansion queryExpansion = new QueryExpansion(analyzer, searcher, similarity, props, "contents");
+						query = queryExpansion.expandQuery(queryStr, hits, props);
+						// Second search using Rocchio's pseudo relevance feedback
+						System.out.println("Searching for : (" + qqs[i].getQueryID()+") "+query);
+						if (k < relevanceFeedbackRuns) {
+							td = doTopHitSearch(searcher, query, 10*k);
+						} else { // k == runs (ready for output)
+							td = doTopHitSearch(searcher, query, 100); //project setting
+						}*/
+					}
+				}
+				
+				// Output
+				PrintWriter writer = new PrintWriter(new FileWriter(rankingFile, true));
+				SubmissionReport report = new SubmissionReport(writer, "test");
+				report.report(qqs[i], td, "DOCNO", searcher);
+				writer.close();
+			}
+			
+			// IR Evaluation
+			String type = "";
+			if (topicsFile.indexOf("training") > -1)
+				type = "training";
+			else
+				type = "testing";
+			String baselineRankingFile = "output/"+type+"/"+type+"_topics_result-[StandardAnalyzer].txt";
+			String judgmentsFile = "dataset/qrels_"+type+"_topics.txt";
+			System.out.println(args);
+			
+			printEvaluation(baselineRankingFile, rankingFile, judgmentsFile);
+			//System.out.println(baselineRankingFile);
+			//System.out.println(rankingFile);
+			//System.out.println(judgmentsFile);
+			
+		} else {
+			System.err.println(usage);
+			analyzer.close();
+			return;
+		}
+		
+	}
+
+	// added by T.Y.Tung
+	private static TopDocs doTopHitSearch(IndexSearcher searcher, Query query, int numHits) throws IOException {		
+		TopScoreDocCollector collector = TopScoreDocCollector.create(numHits, false);
+		searcher.search(query, collector);
+		TopDocs td = collector.topDocs();
+		return td;
+	}
+	
+	// added by T.Y.Tung
+	public static void printEvaluation(String baselineRankingFile, String rankingFile, String judgmentsFile) throws IOException {
+		if ("".equals(baselineRankingFile) || "".equals(rankingFile) || "".equals(judgmentsFile)) {
+			System.err.println("Ranking files or judgment file not exist.");
+			return;
+		}
+		TreeMap<String, ArrayList<RetrievalEvaluator.Document>> ranking = Main.loadRanking(rankingFile);
+		TreeMap<String, ArrayList<Judgment>> judgments = Main.loadJudgments(judgmentsFile);
+		
+		SetRetrievalEvaluator improvedEvaluator = Main.create(ranking, judgments);
+		System.out.println("\n"+Main.singleEvaluation(improvedEvaluator, true));
+		
+		//TreeMap<String, ArrayList<RetrievalEvaluator.Document>> baselineRanking = Main.loadRanking(baselineRankingFile);
+		//SetRetrievalEvaluator baselineEvaluator = Main.create(baselineRanking, judgments);
+		//System.out.println(Main.comparisonEvaluation(baselineEvaluator, improvedEvaluator, baselineRankingFile, rankingFile));
+		
+		System.out.println(rankingFile);
+		DecimalFormat df = new DecimalFormat("#0.0000");
+		System.out.println("MAP = "+df.format(improvedEvaluator.meanAveragePrecision()));
+		System.out.println("NDCG = "+df.format(improvedEvaluator.meanNormalizedDiscountedCumulativeGain()));
+		System.out.println("NDCG15 = "+df.format(improvedEvaluator.meanNormalizedDiscountedCumulativeGain(15)));
+	}
+
+}
