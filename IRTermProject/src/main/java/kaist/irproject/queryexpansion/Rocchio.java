013fdf9ed2aad8c308ecea7a562f1bdaf0aebb65
emilbunk <emilbunk@gmail.com>
2014-06-11 11:59:29 +0900

    Rocchio

--- IRTermProject/src/main/java/kaist/irproject/queryexpansion/Rocchio.java
+++ IRTermProject/src/main/java/kaist/irproject/queryexpansion/Rocchio.java
@@ -6,12 +6,16 @@ import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.ScoreDoc;
 
 public class Rocchio {
-	public BooleanQuery RocchioQuery expander(ArrayList<String> query, ScoreDoc relDocs, ScoreDoc nonRelDoc, double alpha, double beta, double gamma) {
+	public BooleanQuery RocchioQueryExpander(ArrayList<String> query, ScoreDoc relDocs, ScoreDoc nonRelDoc, double alpha, double beta, double gamma) {
 		ArrayList<String> terms = new ArrayList<String>(query);
 		
 		ArrayList<Double> boosts = new ArrayList<Double>();
 		for(int i = 0; i < terms.size(); i++) {
 			boosts.add(1.0);
 		}
+		
+		
+		
+		return null;
 	}
 }
