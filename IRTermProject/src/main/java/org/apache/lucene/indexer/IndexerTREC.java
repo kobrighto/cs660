e478c51d12a3ee44121a76df979f7fcbd3165662
Anders Michael Nielsen <gedenhesten@gmail.com>
2014-06-18 22:37:44 +0900

    Support for LDA - You might need create a file in root called "docs_txt.txt" to avoid errors

--- IRTermProject/src/main/java/org/apache/lucene/indexer/IndexerTREC.java
+++ IRTermProject/src/main/java/org/apache/lucene/indexer/IndexerTREC.java
@@ -1,18 +1,24 @@
 package org.apache.lucene.indexer;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
+
+import java.util.Arrays;
 import java.util.zip.GZIPInputStream;
 
+import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.util.CharArraySet;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.Field;
@@ -24,8 +30,14 @@ import org.apache.lucene.index.IndexWriterConfig.OpenMode;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
+
 import org.jsoup.Jsoup;
 
+
+/**
+ * @author Anders
+ *
+ */
 public class IndexerTREC {
 	public static void main(String args[]) throws IOException {
 		String INDEXPATH = "Index_TREC";
@@ -33,7 +45,7 @@ public class IndexerTREC {
 		String DOCPATH = "B01.gz";
 		
 		Directory dir = FSDirectory.open(new File(INDEXPATH));
-	    Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_48);
+	    Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_48, stopWordsSet);
 	    IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_48, analyzer);
 	    
 	    if (CREATE) {
@@ -58,8 +70,13 @@ public class IndexerTREC {
 		Reader decoder = new InputStreamReader(gzipStream, StandardCharsets.UTF_8);
 		BufferedReader in = new BufferedReader(decoder);
 		
-		String line;
+		File file = new File("docs_txt.txt");
+		FileWriter filerWriterDocs = new FileWriter(file.getAbsoluteFile());
+		BufferedWriter bufferedWriterDocs = new BufferedWriter(filerWriterDocs);
 		
+		
+		String line;
+		int docCount = 1;
 		while ((line = in.readLine()) != null){
 			if(line.equals("<DOC>")){
 				Document doc = new Document();
@@ -72,22 +89,74 @@ public class IndexerTREC {
 				while (!(line = in.readLine()).equals("</DOC>")) {
 					content += line + " ";
 				}
-				
+				content = content.substring(content.indexOf("</DOCHDR>"));
 				content = Jsoup.parse(content).text();
 				
-				doc.add(new TextField("contents", content, Field.Store.YES));
+				doc.add(new TextField("contents", content, Field.Store.YES)); // Content of the doc
+				doc.add(new TextField("indexnumber", Integer.toString(docCount), Field.Store.YES)); // The number which corresponds to the LDA id
 				
 				if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
 		            System.out.println("adding " + docnum);
 		            //System.out.println(doc);
 		            writer.addDocument(doc);
+		            TokenStream tokenStream = writer.getAnalyzer().tokenStream("contents", doc.get("contents"));
+		            CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
+		        	
+				    tokenStream.reset();
+				    int c = 0; // Count number of terms in a document. Used for debugging purposes
+				    String docContent = "";
+				    while (tokenStream.incrementToken()) {
+				        String term = charTermAttribute.toString();
+				        docContent = docContent + " " + term;
+				        System.out.println(term);
+				        
+				        c++;
+				    }
+				    System.out.println("Antal: " + c);
+				    bufferedWriterDocs.write(docContent.substring(1) + "\n");
+				    tokenStream.end();
+				    tokenStream.close();
+				    docCount++; // increases index number
+				    
+				    
 		          } else {
 		            System.out.println("updating " + docnum);
 		            writer.updateDocument(new Term("docnumber", docnum), doc);
 		          }
 			}
+			
 		}
-		
+		bufferedWriterDocs.close();
 		in.close();
 	}
+	
+	// Better stop words then the default. Used for the analyser.
+	static String [] STOP_WORDS = new String [] {
+	       "0", "1", "2", "3", "4", "5", "6", "7", "8",
+	        "9", "000", "$",
+	        "about", "after", "all", "also", "an", "and",
+	        "another", "any", "are", "as", "at", "be",
+	        "because", "been", "before", "being", "between",
+	        "both", "but", "by", "came", "can", "come",
+	        "could", "did", "do", "does", "each", "else",
+	        "for", "from", "get", "got", "has", "had",
+	        "he", "have", "her", "here", "him", "himself",
+	        "his", "how","if", "in", "into", "is", "it",
+	        "its", "just", "like", "make", "many", "me",
+	        "might", "more", "most", "much", "must", "my",
+	        "never", "now", "of", "on", "only", "or",
+	        "other", "our", "out", "over", "re", "said",
+	        "same", "see", "should", "since", "so", "some",
+	        "still", "such", "take", "than", "that", "the",
+	        "their", "them", "then", "there", "these",
+	        "they", "this", "those", "through", "to", "too",
+	        "under", "up", "use", "very", "want", "was",
+	        "way", "we", "well", "were", "what", "when",
+	        "where", "which", "while", "who", "will",
+	        "with", "would", "you", "your",
+	        "a", "b", "c", "d", "e", "f", "g", "h", "i",
+	        "j", "k", "l", "m", "n", "o", "p", "q", "r",
+	        "s", "t", "u", "v", "w", "x", "y", "z"};
+	static CharArraySet stopWordsSet = new CharArraySet(Version.LUCENE_48,Arrays.asList(STOP_WORDS), true); 
+		
 }
